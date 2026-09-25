9a070f18a53f98ebf9cd8d62fe0e912b8d4bd87b
Anthony P. Alicea <hey@tonyalicea.dev>
2026-09-25 08:26:09 -0400

    Brain dump

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = config => {
     config.addPassthroughCopy("normalui");
     config.addPassthroughCopy("ux-context");
     config.addPassthroughCopy("sloptris");
+    config.addPassthroughCopy("brain-dump");
     config.addPassthroughCopy("projects/interface-normalization");
     config.addPassthroughCopy('_redirects');
 
